615d1a44a9b4c211c853adcc26dc294264a9d07b
Benjamin Janssens <benjamin.janssens@persgroep.net>
2026-06-10 17:32:14 +0200

    fix: add verified parameter for cross-domain dl.trycloud.app URL

--- Casks/cloud-app.rb
+++ Casks/cloud-app.rb
@@ -2,7 +2,8 @@ cask "cloud-app" do
   version "0.1.0"
   sha256 "6cba29f05291b4b6f5ef9d088554bdd5fae4ec1c9acf879a6fdaf55e694739a9"
 
-  url "https://dl.trycloud.app/CloudApp-#{version}.dmg"
+  url "https://dl.trycloud.app/CloudApp-#{version}.dmg",
+      verified: "dl.trycloud.app/"
   name "Cloud.app"
   desc "Menu bar app for switching between AWS SSO sessions"
   homepage "https://github.com/benjidotsh/cloud.app"
